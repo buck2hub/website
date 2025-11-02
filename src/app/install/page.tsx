7c050e9a5c53179e913930b673fb5a74347e3212
Quanyi Ma <eli@patch.sh>
2025-11-02 22:38:34 +0800

    Add blog and install pages
    Signed-off-by: Quanyi Ma <eli@patch.sh>

---
+++ src/app/install/page.tsx
@@ -0,0 +1,8 @@
+
+export default function HomePage() {
+  return (
+    <main className="flex flex-1 flex-col justify-center text-center">
+      <h1 className="mb-4 text-2xl font-bold">Hello Blog</h1>
+    </main>
+  );
+}
--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -3,7 +3,29 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'My App',
+      title: 'Buck2hub',
     },
+    links: [
+      {
+        text: 'Install',
+        url: '/install',
+        active: 'nested-url',
+      },
+      {
+        text: 'Documentation',
+        url: '/docs',
+        active: 'nested-url',
+      },
+      {
+        text: 'Blog',
+        url: '/blog',
+        active: 'nested-url',
+      },
+      {
+        text: 'Forge',
+        url: 'https://forge.rk8s.dev',
+        active: 'nested-url',
+      },
+    ],
   };
 }
